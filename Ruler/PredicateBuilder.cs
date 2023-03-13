797350384d551a1c16324a2086a15e0028bb9454
Sean McArdle <sean@mcardletech.com>
2023-03-13 13:07:46 -0700

    Refactored predicatebuilder into it's own file since it has a different copyright.

---
+++ Ruler/PredicateBuilder.cs
@@ -0,0 +1,55 @@
+using System.Linq.Expressions;
+
+namespace Ruler;
+
+// Taken from LinqKit, Joseph Albahari. Man is a legend.
+// LINQKit Copyright (c) 2007-2009 Joseph Albahari, Tomas Petricek
+// 
+// The Expression Visitor class is based on a Microsoft sample.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+public static class PredicateBuilder
+{
+    public static Expression<Func<T, bool>> True<T>()
+    {
+        return f => true;
+    }
+
+    public static Expression<Func<T, bool>> False<T>()
+    {
+        return f => false;
+    }
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1,
+        Expression<Func<T, bool>> expr2)
+    {
+        var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+        return Expression.Lambda<Func<T, bool>>
+            (Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+    }
+
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+        Expression<Func<T, bool>> expr2)
+    {
+        var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+        return Expression.Lambda<Func<T, bool>>
+            (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+    }
+}
